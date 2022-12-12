86efa83a7691d038140e4c19d17009cb7d65747b
Joalisson Gomes <joalissongomes.webdev@gmail.com>
2022-12-12 01:27:03 -0300

    feat: Create Transactions page

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,40 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">
+                  R$ 12.000,00
+                </PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>30/11/2022</td>
+            </tr>
+
+            <tr>
+              <td>Hamburguer</td>
+              <td>
+                <PriceHighLight variant="outcome">
+                  - R$ 59,00
+                </PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/12/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  )
+}
