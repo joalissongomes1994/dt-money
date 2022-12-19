3997cce3aaca0f490f9bcc589607ced59ab70489
Joalisson Gomes <joalissongomes.webdev@gmail.com>
2022-12-19 02:44:42 -0300

    feat: Add functionality to fetch transactions

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -10,7 +10,8 @@ interface Transaction {
 }
 
 interface TransactionsContextType {
-  transactions: Transaction[]
+  transactions: Transaction[],
+  fetchTransaction: (query?: string) => Promise<void>
 }
 
 interface TransactionsContextProps {
@@ -22,16 +23,21 @@ export const TransactionsContext = createContext({} as TransactionsContextType)
 export function TransactionsProvider({children}: TransactionsContextProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
 
-  async function loadTransaction() {
-    const response = await fetch("http://localhost:3333/transactions")
+  async function fetchTransaction(query?: string) {
+    const url = new URL('http://localhost:3333/transactions')
+
+    query && url.searchParams.append('q', query)
+
+    const response = await fetch(url)
     const data = await response.json()
 
     setTransactions(data)
   }
 
-  useEffect(() => { loadTransaction() },[])
+  useEffect(() => { fetchTransaction() },[])
+
   return(
-    <TransactionsContext.Provider value={{transactions}}>
+    <TransactionsContext.Provider value={{transactions, fetchTransaction}}>
       {children}
     </TransactionsContext.Provider>
   )
